ecec94185ac61748d19619f23cf7edc6eb1f530d
Umer farooq <45381610+umerfarooq69@users.noreply.github.com>
2024-06-25 17:42:55 +0500

    subscribe form dynamic

--- src/components/ui/footer/index.jsx
+++ src/components/ui/footer/index.jsx
@@ -1,19 +1,31 @@
 import Image from 'next/image';
+import { useState } from 'react';
 
 import { footerLogo } from '../../../../utilities/static-images-imports';
 
-const Footer = () =>
-  <div className="py-12">
-    <div className="flex justify-between">
-      <Image alt="Footer Logo" src={footerLogo} />
+const Footer = () => {
+  const [email, setEmail] = useState('');
 
-      <div className="flex gap-4">
-        <input className="border-0 h-11 rounded-lg pl-[14px] outline-none" placeholder="Enter your email" type="text" />
-        <button className="text-white bg-tertiary px-[18px] rounded-lg" type="button">{'Subscribe'}</button>
+  const onSubmit = () => {
+    // API Call here
+    // eslint-disable-next-line no-console
+    if (email) console.log(email);
+  };
+
+  return (
+    <div className="py-12">
+      <div className="flex justify-between">
+        <Image alt="Footer Logo" src={footerLogo} />
+
+        <div className="flex gap-4">
+          <input className="border-0 h-11 rounded-lg pl-[14px] outline-none" onChange={({ target: { value } }) => setEmail(value)} placeholder="Enter your email" type="text" value={email} />
+          <button className="text-white bg-tertiary px-[18px] rounded-lg" onClick={() => onSubmit()} type="button">{'Subscribe'}</button>
+        </div>
       </div>
-    </div>
 
-    <p className="text-right mt-16 text-[#D0D5DD]">{'© 2077 Untitled UI. All rights reserved.'}</p>
-  </div>;
+      <p className="text-right mt-16 text-[#D0D5DD]">{'© 2077 Untitled UI. All rights reserved.'}</p>
+    </div>
+  );
+};
 
 export default Footer;
